51df8aa02a98cb1b52f94768ba996e32a5f0d92e
Derek Blair <derekjblair@gmail.com>
2017-09-12 00:36:15 +0200

    adding more prime methods

--- ch1/primality.hs
+++ ch1/primality.hs
@@ -8,3 +8,10 @@ isPrimeWilson x = (product [1..(x-1)]) `mod` x == x-1
 
 -- This is an implementation of Fermat's Little Theorem
 isPrimeLittleFermat x = (product (take x (repeat 2))) `mod` x == 2
+
+
+
+primesLessThan x = [ p | p <- [2..x], isPrimeWilson p]
+
+
+primeNumberFrac x = (log (fromIntegral x))*(fromIntegral (length (primesLessThan x)))/(fromIntegral x)
